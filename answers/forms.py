7b35463de4772962a2839ee9ff53a2d2b1ef60bc
shaneneiman <shane.neiman@gmail.com>
2020-09-10 00:09:24 -0400

    templates built out for questions

---
+++ answers/forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+from .models import Answer
+
+class AnswerForm(ModelForm):
+    class Meta:
+        model = Answer
+        fields = [
+            "answer_body",
+        ]
--- questions/views.py
+++ questions/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Question
+from .models import Question, get_user_questions
 from .forms import QuestionForm
 
 # Create your views here.
@@ -17,19 +17,28 @@ def ask_question(request):
     })
 
 
-def delete_question(request):
-    pass
+def delete_question(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    if request.method="POST":
+        question.delete()
+        return redirect("list_question")
+    return render(request, "questions/delete_question", {
+        "question": question
+    })
 
 
 def list_question(request):
     questions = Question.objects.all()
-    return render(request, "list_question.html", {
+    return render(request, "questions/list_question.html", {
         "questions": questions
     })
 
 
 def list_user_questions(request):
-    pass
+    questions = Question.objects.get_user_questions(self.request.user)
+    return render(request, "questions/list_user_questions", {
+        "questions": questions
+    })
 
 
 def search_question(request):
@@ -40,5 +49,8 @@ def search_question_results(request):
     pass
 
 
-def view_question(request):
-    pass
+def view_question(request, pk):
+    question = Question.objects.get(pk=pk)
+    return render(request, "questions/view_question.html", {
+        "question": question
+    })
